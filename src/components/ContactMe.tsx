173f6ccb88ad5324987ac8b72b6095fea3e81f23
Azizur Rahman <azizurrahmanemon007@gmail.com>
2023-11-27 02:03:04 +0600

    final

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,46 +1,78 @@
-import { EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid'
-import React from 'react'
-type Props = {}
+import { ValidationError, useForm } from '@formspree/react';
+import { EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid';
+import React from 'react';
+
+type Props = {};
 
 const ContactMe = (props: Props) => {
+  const [state, handleSubmit] = useForm("mjvqqyql"); // Replace 'YOUR_FORM_ID' with your actual Formspree form ID
   return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className="absolute top-24 uppercase text-gray-500 text-2xl tracking-[20px]">
-           Contact
-        </h3>
-        <div className="flex flex-col space-y-10">
-            <h4 className="text-4xl font-semibold text-center sm:mt-10">
-                If you are interested about my skill. {" "}
-                <span className="underline decoration-[#16a5e2]/50">Lets Talk.</span>
-            </h4>
-            <div className='space-y-10'>
-                <div className="flex items-center space-x-5">
-                  <PhoneIcon className='text-[#16a5e2] h-7 w-7 animate-pulse'/>  
-                  <p>01606072942</p>
-                </div>
-                <div className="flex items-center space-x-5">
-                  <EnvelopeIcon className='text-[#16a5e2] h-7 w-7 animate-pulse'/>  
-                  <p>azizur15-2533@diu.edu.bd</p>
-                </div>
-                <div className="flex items-center space-x-5">
-                  <MapPinIcon className='text-[#16a5e2] h-7 w-7 animate-pulse'/>  
-                  <p>Uttara,Dhaka,Bangladesh</p>
-                </div>
-            </div>
-            <form 
-            className='flex flex-col space-y-2 w-fit mx-auto'
-            >
-                <div className='flex space-x-2'>
-                    <input placeholder='Name' className='contactInput' type="text" />
-                    <input placeholder='Email' className='contactInput' type="email" />
-                </div>
-                <input placeholder='Subject' className='contactInput' type="text" />
-                <textarea placeholder='Message' className='contactInput'/>
-                <button className='bg-[#16a5e2] px-10 py-5 rounded-md text-black font-bold text-lg' type='submit'>Submit</button>
-            </form>
-        </div>
-    </div>
-  )
-}
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase text-gray-500 text-2xl tracking-[20px]">Contact</h3>
 
-export default ContactMe
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center sm:mt-10">
+          If you are interested about my skill. {' '}
+          <span className="underline decoration-[#16a5e2]/50">Lets Talk.</span>
+        </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5">
+            <PhoneIcon className="text-[#16a5e2] h-7 w-7 animate-pulse" />
+            <p>01606072942</p>
+          </div>
+
+          <div className="flex items-center space-x-5">
+            <EnvelopeIcon className="text-[#16a5e2] h-7 w-7 animate-pulse" />
+            <p>azizur15-2533@diu.edu.bd</p>
+          </div>
+
+          <div className="flex items-center space-x-5">
+            <MapPinIcon className="text-[#16a5e2] h-7 w-7 animate-pulse" />
+            <p>Uttara,Dhaka,Bangladesh</p>
+          </div>
+        </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto" onSubmit={handleSubmit}>
+        <div className="flex space-x-2">
+          <input placeholder="Name" className="contactInput" type="text" name="name" />
+          <ValidationError 
+            prefix="Name" 
+            field="text"
+            errors={state.errors}
+          />
+          <input placeholder="Email" className="contactInput" type="email" name="_replyto" />
+          <ValidationError 
+            prefix="Email" 
+            field="email"
+            errors={state.errors}
+          />
+        </div>
+
+        <input placeholder="Subject" className="contactInput" type="text" name="subject" />
+        <ValidationError 
+          prefix="Subject" 
+          field="text"
+          errors={state.errors}
+        />
+        <textarea placeholder="Message" className="contactInput" name="message" />
+        <ValidationError 
+          prefix="Message" 
+          field="textarea"
+          errors={state.errors}
+        />
+        <button
+          className="bg-[#16a5e2] px-10 py-5 rounded-md text-black font-bold text-lg"
+          type="submit"
+        >
+          Submit
+        </button>
+
+        {state.succeeded && <p className="text-[#d6d6d8]">Thanks for contacting! Will reply soon...</p>}
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
